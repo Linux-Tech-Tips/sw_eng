43219777ed58a38488f6ec6659b71a2bcfde3cdc
Willow Pfeiffer <e.pfeiffer1@universityofgalway.ie>
2025-01-21 19:48:49 +0000

    script with plan for text preprocessor

---
+++ html2txt2.py
@@ -0,0 +1,9 @@
+#check for correct number of arguments
+#create text file with correct title
+#open html file
+#read it in line by line
+#use regex to find <script --> delete each character until we hit </script>
+#use regex to find <
+#delete each next character until we hit >
+#if we don't hit a >, delete each character in the next line until we hit >
+#convert all html entities to the proper characters
