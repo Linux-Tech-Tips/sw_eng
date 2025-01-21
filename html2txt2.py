7bd17e6f6aaa982d18ce3abb85e28440f1181385
Willow Pfeiffer <e.pfeiffer1@universityofgalway.ie>
2025-01-21 21:24:35 +0000

    code for opening file and tests for regular expressions

--- html2txt2.py
+++ html2txt2.py
@@ -7,3 +7,27 @@
 #delete each next character until we hit >
 #if we don't hit a >, delete each character in the next line until we hit >
 #convert all html entities to the proper characters
+
+import sys
+import re
+import html
+
+print(re.search("c", "abcdef"))
+print(re.search("food", "I like foods"))
+
+"""
+if(len(sys.argv)!=2):
+  print("incorrect number of arguments")
+  quit()
+
+inFile = sys.argv[1] #takes the name of the html file as a command line argument
+title = inFile #copy the name of the file and edit it to be .txt instead of .html
+title = title.split(".")
+title[-1]="txt"
+outFile=".".join(title)
+outFile = "./"+outFile
+
+with open(inFile, 'r') as f: #opens the original file for reading
+  for line in f:
+    line = f.readline()#read in each line
+   """ 
