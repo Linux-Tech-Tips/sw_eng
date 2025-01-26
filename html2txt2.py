e8b4ac9bf5fd565d69234cae93b2a21b826a059b
Willow Pfeiffer <e.pfeiffer1@universityofgalway.ie>
2025-01-26 10:34:45 +0000

    added work to process noscript tags and multi-line tags. not fully functional

--- html2txt2.py
+++ html2txt2.py
@@ -15,14 +15,20 @@ outFile=".".join(title)
 
 
 with open(inFile, 'r') as f: #opens the original file for reading
-  with open(outFile, 'w') as w: #opens the new file for writing
-    for line in f:
+  with open(outFile, 'w') as w: #opens the new file for writing  
+    while True:
       line = f.readline()#read in each line
+      if not line:
+        break
       if(re.search("<script", line)!=None): #if there's script tag --> skip to next line until </script> is found
-        while(re.search("</script>", line)!=None):
-          line = f.readline()
+        while(re.search("</script>", line)==None):
+          line = f.readline() 
         line = f.readline() #remove the line with the </script>
       line = re.sub("<.*?>","", line) #removes all text inside <>
+      if(re.search("<", line)!=None): #if there's an open tag --> delete every line until it ends
+        while(re.search(">", line)==None):
+          line = f.readline()
+        line = f.readline() #remove the last line of the tag
       line = html.unescape(line) #process all html entities
       w.write(line)
     w.close()
