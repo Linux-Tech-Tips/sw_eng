0cf0b767249e8889bb5c9f0a4753a0b3984410ab
Willow Pfeiffer <e.pfeiffer1@universityofgalway.ie>
2025-02-19 16:57:36 +0000

    added zipped file of processed word vectors and script for obtaining these

---
+++ backend/vocabularyProcessing/removeWords.js
@@ -0,0 +1,24 @@
+//script that processes the original file of word vectors into a filtered file in JSON formatting
+
+const  fs = require('fs');
+const sw = require('stopword');
+
+let glove = fs.readFileSync('glove2.txt');//glove2.txt is the file with the words+vectors on one line each
+
+glove = glove.toString().split('\n'); //split it into an array by newlines and sort it
+glove.sort();
+
+let gloveArray = {};
+let tempArray = [];
+
+for(let i=0; i<glove.length; i++) { //for all entries in the original array
+  if(glove[i].substring(0, glove[i].indexOf(' ')).match(/^[a-z]+$/g)) { //if the word is entirely english characters 
+    tempArray[0] = glove[i].substring(0, glove[i].indexOf(' '));
+    tempArray = sw.removeStopwords(tempArray); //call removeStopwords on it
+    if(tempArray.length>0) {  //if the word still exists add it to the new array
+      gloveArray[glove[i].substring(0, glove[i].indexOf(' '))] = glove[i].substring(glove[i].indexOf(' ') +1); //set the vector as the entry indexed to the word
+    }
+  }
+}
+
+fs.writeFileSync('glove.txt', JSON.stringify(gloveArray));
