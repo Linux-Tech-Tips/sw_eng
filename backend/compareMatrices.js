4d6d9e004b068239c33973da5247d185104d7024
Willow Pfeiffer <e.pfeiffer1@universityofgalway.ie>
2025-02-24 20:03:10 +0000

    now increases the similarity based on the cosine similarity, and has some tests

--- backend/compareMatrices.js
+++ backend/compareMatrices.js
@@ -13,7 +13,6 @@ function compareMatrices(query, page) {
   for(let i=0; i<query.length; i++) {
     similarity[i]=0;
   }
-  let threshold = 0.5; //threshold for similarity between words EDIT TO TWEAK PERFORMACE
   let importantWord = 0.7; //threshold for when a word is big relevant EDIT TO TWEAK PERFORMANCE  
   let increaseSimilarity = 0.01; //amount to add to the similarity if a word is big relevant COULD MAYBE CHANGE DEPENDING ON WORDCOUNT????  
 
@@ -21,7 +20,8 @@ function compareMatrices(query, page) {
   let magQuery = 0; //magnitude of current query word
   let magPage = 0; //magnitude of current page word
   let cosim = 0; //cosine similarity between current words
-
+  
+  
   for(i=0; i<query.length; i++) {//for all words in the query
     //calculate the magnitude of the current word
     magQuery = query[i].reduce((acc, n) => acc + (n*n), 0);
@@ -31,16 +31,13 @@ function compareMatrices(query, page) {
       //calculate the magnitude of the current word
       magPage = page[i].reduce((acc, n) => acc + (n*n),0);
       magPage = Math.sqrt(magPage);
-      
+
       //calculate the dot product + the cosine similarity
       dotProduct = query[i].reduce((acc, n, k) => acc + (n * page[j][k]), 0);
       cosim = dotProduct/(magPage*magQuery);
-      
-      //if the similarity is high enough --> increment the similarity index of that word
-      if(cosim>threshold) {
-        similarity[i]++;
-      }
+      similarity[i]+=cosim; //increment the similarity index for that word by the cosine similarit
     }
+    //console.log(similarity);
     //divide the similarity of the query word by the number of words in the page
     similarity[i] = similarity[i]/page.length;
   } 
@@ -54,19 +51,43 @@ function compareMatrices(query, page) {
     if(avgSimilarity+increaseSimilarity>1) {
       break;
     }
-    if(similarity[i]>importantWord) {
-      avgSimilarity+=increaseSimilarity;
+    if(similarity[i]>avgSimilarity) { //increase the similarity if a word appears more often than average
+      avgSimilarity+=similarity[i]; //NEEDS TO BE ALTERED --> COULD PUT IT OVER 1
     }
   }
   console.log(avgSimilarity);
 
 }
 
-function sumSquares(acc, n){
-  return acc += n*n;
+function genMatrices(length, min, max) {
+  let v = new Array(length).fill(new Array(50).fill(0));
+  for(let j=0; j<length; ++j) {  
+    for(let i=0; i<50; ++i) {
+      v[j][i] = Math.random() * (max - min) + min;
+     }
+  }
+  return v;
 }
 
+function stressTest(size, min, max) {
+  let v1 = genMatrices(10,min, max);
+  let v2 = genMatrices(size,min, max); 
+  
+  compareMatrices(v1, v2);
+  console.log("Run with a page wordcount of=" + size + "; min=" + min + "; max=" + max);
+}
+
+let t0 = performance.now();
+stressTest(1000, 0, 1000);
+stressTest(1000, 0, 1000);
+stressTest(1000, 0, 1000);
+stressTest(1000, 0, 1000);
+stressTest(1000, 0, 1000);
+let t1 = performance.now();
+console.log("Performance: " + (t1-t0) + "ms");
+
+/*
 let mA = [[1,2,3],[2,3,1], [3,2,1]];
 let mB = [[1,2,3],[-2,-3,-1], [-3,-2,-1]];
 compareMatrices(mA, mB);
-
+*/
