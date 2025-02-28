b4a6f1e38bf5c3327263264a1adb3d855693d4d2
Willow Pfeiffer <e.pfeiffer1@universityofgalway.ie>
2025-02-28 18:35:17 +0000

    removed more words from the file

--- backend/vocabularyProcessing/removeWords.js
+++ backend/vocabularyProcessing/removeWords.js
@@ -4,15 +4,14 @@ module.export = {uploadVocab};
 
 async function uploadVocab() {
 
-const db = require('dbUtil');
+const db = require('../dbUtil');
 const  fs = require('fs');
 const sw = require('stopword');
 
-let glove = fs.readFileSync('/Users/elisabeth/Desktop/Year2/SWE/SWE/glove2.txt');//glove2.txt is the file with the words+vectors on one line each
+let glove = fs.readFileSync('/Users/elisabeth/Desktop/Year2/SWE/SWE/semiProcessedVectors.txt');//glove2.txt is the file with the words+vectors on one line each
 
 glove = glove.toString().split('\n'); //split it into an array by newlines and sort it
 glove.sort();
-
 
 let gloveArray = {};
 let tempArray = [];
@@ -21,12 +20,16 @@ for(let i=0; i<glove.length; i++) { //for all entries in the original array
   if(glove[i].substring(0, glove[i].indexOf(' ')).match(/^[a-z]+$/g)) { //if the word is entirely english characters 
     tempArray[0] = glove[i].substring(0, glove[i].indexOf(' '));
     tempArray = sw.removeStopwords(tempArray); //call removeStopwords on it
-    if(tempArray.length>0) {  //if the word still exists add it to the new array
-      await db.dbSetWord(glove[i].substring(0, glove[i].indexOf(' ')), glove[i].substring(glove[i].indexOf(' ') +1).split(' ')); //add the word to the database
-      //gloveArray[glove[i].substring(0, glove[i].indexOf(' '))] = glove[i].substring(glove[i].indexOf(' ') +1).split(' '); //set the vector as the entry indexed to the word
+  }
+  if(tempArray.length>0 && tempArray[0].length>2 && tempArray[0].substring(0,1)!=tempArray[0].substring(1,2)) {  //if the word exists, is longer than 2 characters and the first two characters aren't identical--> add it to the new array
+      //await db.dbSetWord(glove[i].substring(0, glove[i].indexOf(' ')), glove[i].substring(glove[i].indexOf(' ') +1).split(' ')); //add the word to the database
+      gloveArray[glove[i].substring(0, glove[i].indexOf(' '))] = glove[i].substring(glove[i].indexOf(' ') +1).split(' '); //set the vector as the entry indexed to the word
       
     }
   }
+
+
+fs.writeFileSync('glove.txt', JSON.stringify(gloveArray));
 }
-}
-//fs.writeFileSync('glove.txt', JSON.stringify(gloveArray));
+
+uploadVocab();
