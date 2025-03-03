0c780e40481c60b43b4fdc80aacd8429ee29816e
Martin Klacer <m.klacer1@universityofgalway.ie>
2025-03-03 14:42:30 +0000

    Added progress to addPage.js

--- backend/addPage.js
+++ backend/addPage.js
@@ -2,28 +2,47 @@
 
 const dbUtil = require("./dbUtil.js");
 const textProcessing = require("./textProcessing.js");
+const metadata = require("./metadata.js");
 
 /** Takes in a string baseUrl to a website, finds any viable subpages, processes them and adds the processed data into the database */
 async function addPage(baseUrl) {
 
     //let pages = ...
     // TODO Populate this once function to get list of HTML content found
+    // -> Assuming 'pages' being an array of strings containing HTML data
     let pages = [];
 
     /* Preprocess pages, strip html and then save to plaintext */
     for(idx in pages) {
-	pages[idx] = textProcessing.htmlToText(pages[idx]);
-	pages[idx][1] = textProcessing.stripText(pages[idx][1]);
+	let page = textProcessing.htmlToText(pages[idx]);
+	let pageTitle = page[0];
+	let strippedTitle = textProcessing.stripText(pageTitle);
+	let content = textProcessing.stripText(page[1]);
+	pages[idx] = [pageTitle, strippedTitle, content];
     }
 
-    let lastPageID = dbUtil.dbGetLastID();
+    let pageID = await dbUtil.dbGetLastID();
 
     /* Go through preprocessed pages, save into database */
     for(page of pages) {
-	// TODO Based on the Crawler, figure out approach to get page URL as well, then finish implementation here
+	/* Increment ID of last page added */
+	++pageID;
+
+	/* Add page to database */
+	await dbUtil.dbSetPage(page /* TODO figure out URL */, baseUrl, new Date(), page[0], pageID);
+
+	/* Process metadata and add to database */
+	let termFreq = await metadata.metadataProcess([page[1], page[2]]);
+	await dbUtil.dbSetPageMetadata(pageID.toString(), termFreq);
+
+	/* Process meaning and add to database */
+	/* TODO WAITING FOR FUNCTION */
+
+	/* Add content to database */
+	await dbUtil.dbSetPageContent(pageID.toString(), page[2]);
     }
 
-    dbUtil.dbSetLastID(lastPageID);
+    await dbUtil.dbSetLastID(pageID);
 
     // TODO Based on how the Crawler works, this will differ - either JS array of HTML pages, or list of crawlable sites for which HTML will be obtained independently
     // Steps:
@@ -32,5 +51,4 @@ async function addPage(baseUrl) {
     //    -> throw the plaintext into each of the three preprocessors, function which will return a JS object
     //    -> save JS object from each preprocessor into the appropriate database
     //    -> or, alternatively, let each preprocessor do the saving themselves
-
 }
