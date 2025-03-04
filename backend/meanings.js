20fd380f9f62de565cfd2a9299c230e34412c9c6
Willow Pfeiffer <e.pfeiffer1@universityofgalway.ie>
2025-03-04 12:21:47 +0000

    edited meaningSearch to feature the confidence value

--- backend/meanings.js
+++ backend/meanings.js
@@ -27,7 +27,9 @@ function stringToMatrix(text) {
   return vectorisedText;
 }
 
-//function that takes two matrices and returns a number within [0,1] representing how similar they are. TBD: Also returns a value representing the confidence
+//function that takes a matrix query and a JSON object page
+//the first entry in both the query matrix and the page object are a float representation of the perventage of words known
+//returns an array, first value is the confidence value (based on words known) and second is the similarity score
 
 function meaningSearch(query, page) {
   let similarity = []; //matrix for the similarity of each word
@@ -38,15 +40,14 @@ function meaningSearch(query, page) {
   let magQuery = 0; //magnitude of current query word
   let magPage = 0; //magnitude of current page word
   let cosim = 0; //cosine similarity between current words
-  
-  
-  for(i=0; i<query.length; i++) {//for all words in the query
-    //TBD: remember that the percentage of words known is part of the array!!
+  let confidenceValue = (query[0]+page[0])/2 //confidence value is the average of words known 
+ 
+  for(i=1; i<query.length; i++) {//for all words in the query
     //calculate the magnitude of the current word
     magQuery = query[i].reduce((acc, n) => acc + (n*n), 0);
     magQuery = Math.sqrt(magQuery);
 
-    for(let j=0; j<page.length; j++) { //for each word in the page
+    for(let j=1; j<page.length; j++) { //for each word in the page
       //calculate the magnitude of the current word
       magPage = page[i].reduce((acc, n) => acc + (n*n),0);
       magPage = Math.sqrt(magPage);
@@ -63,7 +64,6 @@ function meaningSearch(query, page) {
   
   //calculate the average of the similarity of all query words
   let avgSimilarity = similarity.reduce((acc, n) => acc + n, 0)/similarity.length;
-  console.log(avgSimilarity);
 
   //loop through array, if any words are big important --> add to the similarity
   for(i=0; i<similarity.length; i++) {
@@ -71,10 +71,9 @@ function meaningSearch(query, page) {
       break;
     }
     if(similarity[i]>avgSimilarity) { //increase the similarity if a word appears more often than average
-      avgSimilarity+=similarity[i]; //TBD: NEEDS TO BE ALTERED --> COULD PUT IT OVER 1
-    }
+      avgSimilarity+=similarity[i]/similarity.length;
   }
-  return avgSimilarity; //TBD: needs to calculate smth with the percentages of words known
-
+  let output = [confidenceValue, avgSimilarity];
+  return output; 
 }
 
