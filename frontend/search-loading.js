ebc8ed64449eff578292e2751f95280450f89dce
Lilian <lilian.themill@icloud.com>
2025-04-01 20:53:42 +0100

    Update search-loading.js

--- frontend/search-loading.js
+++ frontend/search-loading.js
@@ -1,15 +1,5 @@
-const searchButton = document.getElementById('search-button');
-const searchBox = document.getElementById('search-box');
-const searchLoadingOverlay = document.getElementById('search-loading-overlay');
-
-searchButton.addEventListener('click', function() {
-    searchLoadingOverlay.style.display = 'flex';
-
-    setTimeout(function() {
-        searchLoadingOverlay.style.display = 'none';
-
-        setTimeout(() => {
-            window.location.href = "results.html?query=" + encodeURIComponent(searchBox.value);
-        });
-    }, 4000);
-});
+function sendSearch(searchBoxId, resultsPage) {
+    const searchBox = document.getElementById(searchBoxId);
+    const query = encodeURIComponent(searchBox.value);
+    window.location.href = resultsPage + "?query=" + query; // Send to results page immediately
+}
