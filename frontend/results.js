da3f6b2acf66cdc9bca277e50a1686367b417344
Lilian <lilian.themill@icloud.com>
2025-04-01 20:54:54 +0100

    Added results.js

---
+++ frontend/results.js
@@ -0,0 +1,44 @@
+function getSearch() {
+    const query = new URLSearchParams(window.location.search);
+    query.get();
+}
+
+// Displays until results are retrieved from backend??
+function displayLoadingOverlay(loadingOverlayId) {
+    const searchLoadingOverlay = document.getElementById(loadingOverlayId);
+
+    searchLoadingOverlay.style.display = 'flex';
+    searchLoadingOverlay.style.display = 'none';
+
+}
+
+async function processSearch(loadingOverlayId, resultsDivId, firebaseURL) {
+    const searchLoadingOverlay = document.getElementById(loadingOverlayId);
+
+    searchLoadingOverlay.style.display = 'flex';
+
+    let query = new URLSearchParams(window.location.search).get("query");
+
+    if(!query) {
+        window.location.href = 'index.html';
+    }
+
+    try {
+
+        let response = await fetch(firebaseURL + "?query=" + query);
+        let results = await response.json();
+
+        const resultsDiv = document.getElementById(resultsDivId);
+        for(result of results) {
+            let resultString = "<div class=\"result-item\"> \
+                                    <h2 class=\"result-title\"><a href=\"" + unescape(result.pageUrl) + "\" class=\"result-link\">" + result.pageTitle + "</a></h2> \
+                                </div>";
+            resultsDiv.innerHTML += resultString;
+        }
+
+        searchLoadingOverlay.style.display = 'none';
+
+    } catch(error) {
+        window.location.href = 'index.html';
+    }
+}
